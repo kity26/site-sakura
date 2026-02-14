ef025997086d7c94bb6815439d8fa852354210d8
daimaun <manoewinter4@gmail.com>
2026-02-14 21:21:22 +0100

    Deploy via main/docs and disable auto Actions

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/site-sakura/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
